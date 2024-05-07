c40a0e40b0a4aa0561969fc66059b8d870fe295b
The Pioneer <116901962+ThePioneerJP@users.noreply.github.com>
2024-05-07 23:53:13 +0900

    win対応

--- niwatoko/cli.py
+++ niwatoko/cli.py
@@ -2,12 +2,16 @@ import click
 import os
 import openai
 from niwatoko.foundation_model.interpretation.llm.claude import generate_response
+from niwatoko.foundation_model.interpretation.llm.gpt import generate_response as gpt_generate_response
+import niwatoko
 
 @click.command()
-@click.argument('file_path', type=click.Path(exists=True))
-@click.option('-m', '--model', type=click.Choice(['openai', 'claude']), default='openai', help='使用するモデルを選択します。')
+@click.argument('file_path', type=click.Path(exists=True), required=False)
+@click.option('-m', '--model', type=click.Choice(['openai', 'claude']), default='claude', help='使用するモデルを選択します。')
 @click.option('-o', '--output', type=click.Path(), help='生成されたコードの出力先ファイルを指定します。')
-def main(file_path, model, output):
+@click.option('-v', '--version',  is_flag=True, help='バージョン情報を表示します。')
+
+def main(file_path, model, output, version):
     """
     自然言語のソースコードを読み込んで実行するコマンドラインインターフェース。
 
@@ -15,32 +19,34 @@ def main(file_path, model, output):
         file_path (str): 自然言語のソースコードが書かれたファイルのパス。
         model (str): 使用するモデル（OpenAIまたはClaude）。
         output (str): 生成されたコードの出力先ファイルのパス。
+        version (bool): バージョン情報を表示するかどうか。
     """
-    with open(file_path, 'r') as file:
+    if version:
+        try:
+            print(f"niwatoko version: {niwatoko.__version__}")
+        except AttributeError:
+            print("バージョン情報がniwatokoモジュールに存在しません。")
+        return
+    if not file_path:
+        print("ファイルパスが指定されていません。")
+        return
+
+    with open(file_path, 'r', encoding = "utf-8") as file:
         natural_language_code = file.read()
 
     if model == 'openai':
-        openai.api_key = os.getenv("OPENAI_API_KEY")
-        prompt = f"{natural_language_code}"
-        response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=100,
-            n=1,
-            stop=None,
+        generated_code = gpt_generate_response(
+            model="gpt-4-turbo-2024-04-09",
+            prompt=natural_language_code,
+            max_tokens=1000,
             temperature=0.5,
         )
-        generated_code = response.choices[0].text.strip()
     elif model == 'claude':
         prompt = f"{natural_language_code}"
         print(natural_language_code)
         print("=================================")
         generated_code = generate_response(
-            # model='claude-3-haiku-20240307',
             model='claude-3-sonnet-20240229',
-            # claude-3-sonnet-20240229
-            # claude-3-haiku-20240307
-            # claude-3-opus-20240229
             prompt=prompt,
             max_tokens=4000,
             temperature=0.2,
@@ -48,12 +54,17 @@ def main(file_path, model, output):
     else:
         raise ValueError(f"無効なモデル: {model}")
 
-    # print("生成されたPythonコード:")
-    # print(generated_code)
-
     if output:
-        with open(output, 'w') as file:
+        with open(output, 'w', encoding = "utf-8") as file:
             file.write(generated_code)
             print(f"生成されたコードを {output} に書き出しました。")
 
-    # exec(generated_code)
+
+#     match = re.search(r'version=[\'\"](.+?)[\'\"]', content)
+#     if match:
+#         version = match.group(1)
+#     else:
+#         raise ValueError("バージョン情報が見つかりませんでした。")
+
+#     return version
+
